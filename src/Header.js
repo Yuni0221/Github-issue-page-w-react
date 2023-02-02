72c742376f5ac1c348487f378ed58e1f002ecd2c
Yuni0221 <moonrise002002@gmail.com>
2023-02-02 12:41:13 +0900

    new update

--- src/Header.js
+++ src/Header.js
@@ -2,39 +2,43 @@ import styles from "./Header.module.css";
 
 import Button from "./components/Button";
 import Space from "./components/Space";
+import Tabs from "./components/Tabs";
 
 export default function Header() {
   return (
     <div className={styles.header}>
-      <Button
-        styles={{
-          fontSize: "14px",
-          backgroundColor: "transparent",
-          color: "black",
-        }}
-      >
-        Watch
-      </Button>
-      <Space />
-      <Button
-        styles={{
-          fontSize: "14px",
-          backgroundColor: "transparent",
-          color: "black",
-        }}
-      >
-        Fork <div className={styles.circle}>5</div>
-      </Button>
-      <Space />
-      <Button
-        styles={{
-          fontSize: "14px",
-          backgroundColor: "transparent",
-          color: "black",
-        }}
-      >
-        Star
-      </Button>
+      <div className={styles.buttonContainer}>
+        <Button
+          styles={{
+            fontSize: "14px",
+            backgroundColor: "transparent",
+            color: "black",
+          }}
+        >
+          Watch
+        </Button>
+        <Space />
+        <Button
+          styles={{
+            fontSize: "14px",
+            backgroundColor: "transparent",
+            color: "black",
+          }}
+        >
+          Fork <div className={styles.circle}>5</div>
+        </Button>
+        <Space />
+        <Button
+          styles={{
+            fontSize: "14px",
+            backgroundColor: "transparent",
+            color: "black",
+          }}
+        >
+          Star
+        </Button>
+      </div>
+      <Tabs />
     </div>
   );
 }
