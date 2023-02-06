be413094ccbd22f35ac3ed6798ffd342c7d440c0
Yuni0221 <moonrise002002@gmail.com>
2023-02-06 12:55:21 +0900

    new update

--- src/ListContainer.js
+++ src/ListContainer.js
@@ -4,53 +4,66 @@ import Button from "./components/Button";
 import ListItem from "./components/ListItem";
 import ListItemLayout from "./components/ListItemLayout";
 import Modal from "./components/Modal";
+import Pagination from "./components/Pagination";
 
 import { useState } from "react";
 
 export default function ListContainer() {
   const [inputValue, setInputValue] = useState("is:pr is:open");
   const [list, setList] = useState([]);
+  const [page, setPage] = useState(1);
+
+  // const MAX_PAGE = getData().totalCount
 
   return (
-    <div className={styles.listContainer}>
-      <div className={styles.topSection}>
-        <input
-          className={styles.input}
-          value={inputValue}
-          onChange={(e) => setInputValue(e.target.value)}
-        />
-        <Button
-          style={{
-            fontSize: "14px",
-            backgroundColor: "GreenYellow",
-            color: "black",
-          }}
-        >
-          New Issue
-        </Button>
-      </div>
-      <OpenClosedFilters />
-      <ListItemLayout className={styles.listFilter}>
-        <ListFilter onChangeFilter={(filteredData) => {}} />
-      </ListItemLayout>
-      <div className={styles.container}>
-        {list.map((listItem) => (index) => (
-          <ListItem
-            key={index}
-            badges={[
-              {
-                color: "red",
-                title: "Bug2",
-              },
-              {
-                color: "blue",
-                title: "Bug1",
-              },
-            ]}
+    <>
+      <div className={styles.listContainer}>
+        <div className={styles.topSection}>
+          <input
+            className={styles.input}
+            value={inputValue}
+            onChange={(e) => setInputValue(e.target.value)}
           />
-        ))}
+          <Button
+            style={{
+              fontSize: "14px",
+              backgroundColor: "GreenYellow",
+              color: "black",
+            }}
+          >
+            New Issue
+          </Button>
+        </div>
+        <OpenClosedFilters />
+        <ListItemLayout className={styles.listFilter}>
+          <ListFilter onChangeFilter={(filteredData) => {}} />
+        </ListItemLayout>
+        <div className={styles.container}>
+          {list.map((listItem) => (index) => (
+            <ListItem
+              key={index}
+              badges={[
+                {
+                  color: "red",
+                  title: "Bug2",
+                },
+                {
+                  color: "blue",
+                  title: "Bug1",
+                },
+              ]}
+            />
+          ))}
+        </div>
       </div>
-    </div>
+      <div>
+        <Pagination
+          maxPage={10}
+          currentPage={page}
+          onClickPageButton={(number) => setPage(number)}
+        />
+      </div>
+    </>
   );
 }
 
