880ee39c9febc88cf5ae672f2a591336605eede1
Yuni0221 <moonrise002002@gmail.com>
2023-02-08 00:53:28 +0900

    new update

--- src/pages/CreateIssue.js
+++ src/pages/CreateIssue.js
@@ -1,35 +1,92 @@
+import { useEffect, useRef, useState } from "react";
 import cx from "clsx";
 
 import Button from "../components/Button";
 import styles from "./CreateIssue.module.css";
+import TextField from "../components/TextField";
 
 export default function CreateIssue() {
+  const inputRef = useRef();
+  const textareaRef = useRef();
+  const [inputValues, setInputValues] = useState({ title: "", body: "" });
+  const [errors, setErrors] = useState({});
+  const [isSubmitting, setIsSubmitting] = useState(false);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    const validateResult = validate(inputValues);
+    setErrors(validateResult);
+
+    const refs = { title: inputRef, body: textareaRef };
+    const errorKeys = Object.keys(validateResult);
+
+    if (errorKeys.length !== 0) {
+      const key = errorKeys[0];
+      alert(validateResult[key]);
+      refs[key].current.focus();
+
+      setIsSubmitting(false);
+      return;
+    }
+
+    if (errorKeys.length === 0) {
+      console.log("Submit 성공");
+    }
+  }
+
+  function onChange(e) {
+    const { name, value } = e.target;
+    setInputValues({ ...inputValues, [name]: value });
+  }
+
   return (
     <div className={styles.container}>
       <div className={styles.avatar}></div>
       <div className={cx(styles.inputWrapper, styles.border)}>
-        <input
-          className={cx(styles.input, styles.border)}
-          placeholder="TITLE"
-        ></input>
-        <textarea
-          className={cx(styles.input, styles.textarea, styles.border)}
-          placeholder="Leave a comment"
-        ></textarea>
-        <div className={styles.buttonWrapper}>
-          <Button
-            style={{
-              fontSize: "14px",
-              backgroundColor: "Aquamarine",
-              color: "black",
-            }}
-          >
-            Submit new issue
-          </Button>
-        </div>
+        <form onSubmit={handleSubmit}>
+          <TextField
+            ref={inputRef}
+            name="title"
+            placeholder="TITLE"
+            value={inputValues.title}
+            onchange={onChange}
+            error={errors.title}
+          />
+          <TextField
+            type="textarea"
+            ref={textareaRef}
+            name="body"
+            placeholder="Leave a comment"
+            value={inputValues.body}
+            onChange={onChange}
+            error={errors.body}
+          />
+
+          <div className={styles.buttonWrapper}>
+            <Button
+              type="submit"
+              style={{
+                fontSize: "14px",
+                backgroundColor: "Aquamarine",
+                color: "black",
+              }}
+              disabled={isSubmitting}
+            >
+              Submit new issue
+            </Button>
+          </div>
+        </form>
       </div>
     </div>
   );
 }
 
-// export default CreateIssue;
+function validate(values) {
+  let errors = {};
+  if (values.title === "") {
+    errors = { title: "타이틀은 필수값입니다." };
+  }
+
+  return errors;
+}
